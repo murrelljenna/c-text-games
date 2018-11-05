ce1b7e16e6b2938525767437f910838ee661f696
Jenna Murrell <murrelljenna@gmail.com>
2018-11-05 15:00:08 -0500

    Battleship.c now has a master double pointer that points to the 4 boards used in the game. This will allow the tlib.c and pst.c commands to iterate through each board and perform all necessary functions without having to call each function multiple times for each board.

--- battleship.c
+++ battleship.c
@@ -4,14 +4,30 @@
 #include "tiles.h"
 
 #define SIZE 100
+#define PLAYERS 2
+#define BOARDS 4
 
 int main(void){
-	
+	int i;	
+
 	struct Tile *player0Board = makeBoard(SIZE);
 	struct Tile *player0Tracker = makeBoard(SIZE);
 	struct Tile *player1Board = makeBoard(SIZE);
-	struct Tile *player1Tracker = makeBoard(SIZE);
+	struct Tile *player1Tracker = makeBoard(SIZE);	
 	
+	struct Tile **boards = malloc((sizeof(struct Tile) * 100) * BOARDS);
+	boards[0] = player0Board;
+	boards[1] = player1Board;
+	boards[2] = player0Tracker;
+	boards[3] = player1Tracker;
+
+	boards[0][0].mark = 'X';
+	boards[3][0].mark = 'X';
+
+	for (i = 0; i < BOARDS; i++){
+		printBoard(boards[i], SIZE);
+	}
+/*
 	printBoard(player0Board, SIZE);
 	printf("\n\n");
 	printBoard(player0Tracker, SIZE);
@@ -19,16 +35,24 @@ int main(void){
 	printBoard(player1Board, SIZE);
 	printf("\n\n");
 	printBoard(player1Tracker, SIZE);
-	
+*/	
 	// Ship setups
 
 	int ships[5] = {2, 3, 3, 4, 5};
+	int player;
+/*
+	for (player = 0; player<PLAYERS; player++) {
+		printf("\n\nPlayer %d, please begin placing your ships\n\n", player);
 		
-
-	for (player = 0; player
-	for (ship = 0; ship < ships; ship++){
-		printf(
-	}			
-
+		for (ship = 0; ship < ships; ship++){
+			printf(
+		}			
+	}
 	return 0;
+*/
+	free(boards);
+	free(player0Board);
+	free(player1Board);
+	free(player0Tracker);
+	free(player1Tracker);
 }
